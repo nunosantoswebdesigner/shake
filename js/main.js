900ba280b15fb14454f127bc7a8020a95d2bbf02
Nuno Santos <42045537+nunosantoswebdesigner@users.noreply.github.com>
2019-07-10 17:47:58 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,43 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+    
+    $(".thankyou_message").hide();
+    $(".success-slide").hide();
+    // swiper for slide views
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        slidesPerView: 1,
+        paginationClickable: true,
+        spaceBetween: 30,
+        keyboardControl: true
+    });
+    $("input").focus(function(){
+        $("#footer_image_letters_on").hide();
+    });
+    
+    //listen to shake event
+    var shakeEvent = new Shake({
+        threshold: 15
+    });
+    
+    shakeEvent.start();
+    
+    window.addEventListener('shake', function () {
+        $("#my_form").submit();
+        $(".first-slide").hide();
+        $(".second-slide").hide();
+        $(".success-slide").show();
+        // stopShake();
+    }, false);
+    
+    //stop listening
+    function stopShake() {
+        shakeEvent.stop();
+    }
+    
+    //check if shake is supported or not.
+    if (!("ondevicemotion" in window)) {
+        alert("Not Supported");
+    }
+    
+});
