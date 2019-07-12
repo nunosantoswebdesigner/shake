15334c415e6aea9c3ce29f5c164dd81363df5675
nunosantoswebdesigner <nunosantoswebdesigner@gmail.com>
2019-07-12 10:44:19 +0100

    teste 3

--- js/form-submission-handler.js
+++ js/form-submission-handler.js
@@ -1,9 +1,25 @@
 (function() {
+
+    
+    $("input").focus(function(){
+        $("#footer_image_letters_on").hide();
+    });
+
     function validEmail(email) {
       var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
       return re.test(email);
     }
   
+    function validateHuman(honeypot) {
+      if (honeypot) {  //if hidden form filled up
+        console.log("Robot Detected!");
+        return true;
+      } else {
+        console.log("Welcome Human!");
+      }
+    }
+  
+    // get all data in form and return object
     function getFormData(form) {
       var elements = form.elements;
   
--- js/main.js
+++ js/main.js
@@ -1,47 +1,47 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
-    $(".thankyou_message").hide();
+// $( document ).ready(function() {
+//     console.log( "ready!" );
+//     $(".thankyou_message").hide();
 
-    var swiper = new Swiper('.swiper-container', {
-        pagination: '.swiper-pagination',
-        slidesPerView: 1,
-        paginationClickable: true,
-        spaceBetween: 30,
-        keyboardControl: true
-    });
-    console.log(swiper)
+//     var swiper = new Swiper('.swiper-container', {
+//         pagination: '.swiper-pagination',
+//         slidesPerView: 1,
+//         paginationClickable: true,
+//         spaceBetween: 30,
+//         keyboardControl: true
+//     });
+//     console.log(swiper)
 
-    $("input").focus(function(){
-        $("#footer_image_letters_on").hide();
-    });
+//     $("input").focus(function(){
+//         $("#footer_image_letters_on").hide();
+//     });
     
-    //listen to shake event
-    var shakeEvent = new Shake({
-        threshold: 15
-    });
+//     //listen to shake event
+//     var shakeEvent = new Shake({
+//         threshold: 15
+//     });
     
-    shakeEvent.start();
+//     shakeEvent.start();
     
-    window.addEventListener('shake', function () {
-        setInterval( () => {
+//     window.addEventListener('shake', function () {
+//         setInterval( () => {
 
-            $(".thankyou_message").show();
-            $(".first-slide").hide();
-            $(".second-slide").hide();
-        }, 4500)
-        $("#my_form").submit().delay('5000');
+//             $(".thankyou_message").show();
+//             $(".first-slide").hide();
+//             $(".second-slide").hide();
+//         }, 4500)
+//         $("#my_form").submit().delay('5000');
 
-        stopShake();
-    }, false);
+//         stopShake();
+//     }, false);
     
-    //stop listening
-    function stopShake() {
-        shakeEvent.stop();
-    }
+//     //stop listening
+//     function stopShake() {
+//         shakeEvent.stop();
+//     }
     
-    //check if shake is supported or not.
-    if (!("ondevicemotion" in window)) {
-        alert("Not Supported");
-    }
+//     //check if shake is supported or not.
+//     if (!("ondevicemotion" in window)) {
+//         alert("Not Supported");
+//     }
     
-});
+// });
